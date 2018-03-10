1a81e470c81e90d7a4b273cd82918a04876d6e85
Martin Sauve <martin.sauve@gmail.com>
2018-03-10 15:58:40 -0500

    hardcoded for now

--- coolstore-ui/app/services/catalog.js
+++ coolstore-ui/app/services/catalog.js
@@ -5,7 +5,7 @@ angular.module("app")
 .factory('catalog', ['$http', '$q', 'COOLSTORE_CONFIG', 'Auth', '$location', function($http, $q, COOLSTORE_CONFIG, $auth, $location) {
 	var factory = {}, products, baseUrl;
 	
-    baseUrl="http://"COOLSTORE_CONFIG.APICAST_ENDPOINT+"/api/products?USER_KEY="+COOLSTORE_CONFIG.USER_KEY;
+    baseUrl="http://coolstore-prod-apicast.apps.myocp.net/api/products?USER_KEY="+COOLSTORE_CONFIG.USER_KEY;
     factory.getProducts = function() {
 		var deferred = $q.defer();
         if (products) {
