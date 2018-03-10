5daad44ef06a13e55e566dff56944305c4717fda
Martin Sauve <martin.sauve@gmail.com>
2018-03-10 15:28:02 -0500

    3scale config

--- coolstore-ui/app/services/catalog.js
+++ coolstore-ui/app/services/catalog.js
@@ -4,14 +4,8 @@ angular.module("app")
 
 .factory('catalog', ['$http', '$q', 'COOLSTORE_CONFIG', 'Auth', '$location', function($http, $q, COOLSTORE_CONFIG, $auth, $location) {
 	var factory = {}, products, baseUrl;
-
-	if ($location.protocol() === 'https') {
-		baseUrl = (COOLSTORE_CONFIG.SECURE_API_ENDPOINT.startsWith("https://") ? COOLSTORE_CONFIG.SECURE_API_ENDPOINT : "https://" + COOLSTORE_CONFIG.SECURE_API_ENDPOINT + '.' + $location.host().replace(/^.*?\.(.*)/g,"$1")) + '/api/products';
-	} else {
-		baseUrl = (COOLSTORE_CONFIG.API_ENDPOINT.startsWith("http://") ? COOLSTORE_CONFIG.API_ENDPOINT : "http://" + COOLSTORE_CONFIG.API_ENDPOINT + '.' + $location.host().replace(/^.*?\.(.*)/g,"$1")) + '/api/products';
-	}
-
-    baseUrl=baseUrl+"?USER_KEY"+COOLSTORE_CONFIG.USER_KEY;
+	
+    baseUrl=COOLSTORE_CONFIG.APICAST_ENDPOINT+"/api/products?USER_KEY"+COOLSTORE_CONFIG.USER_KEY;
     factory.getProducts = function() {
 		var deferred = $q.defer();
         if (products) {
